8ccc7c89560e8bc7f3861e9ef06186b5c0c3109c
Alexisca <150233709+Thealexisca22@users.noreply.github.com>
2025-07-22 17:03:11 +0000

    mentoria

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,57 @@
-import React from "react";
-
+import React, { useState } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+
+//create your first component
+
+
+const Home = () => {
+
+	const [input, setInput] = useState("")
+	const [compra, setCompra] = useState([])
+
+	const handleInput = (e) => {
+		setInput(e.target.value)
+		console.log(input)
+	}
+
+
+	const enviar = (e) => {
+		if (e.key === "Enter") {
+			setCompra([...compra, input])
+			console.log(compra)
+		}
+
+
+	}
+
+	return (
+		<div className="container">
+			<div className="mx-auto row border border-black border-1 justify-content-center" style={{ maxWidth: "800px" }}>
+
+				<h1 className="col-6 text-center">Lista de la compra</h1>
+
+				<form className="col-8 row justify-content-center ">
+					<input
+						className="form-control"
+						placeholder="Producto para comprar"
+						type="text"
+						style={{ width: "680px",}}
+						onKeyUp={(e)=>enviar(e)}
+						value={input}
+						onChange={(e)=>handleInput(e)}
+
+					/>
+				</form>
+				<ul className="col-8 row justify-content-center mt-3">
+					{compra.map((element, index) => (
+						<li key={index}>{element}</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 };
